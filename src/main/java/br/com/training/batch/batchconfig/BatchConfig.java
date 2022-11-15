a45e65de1ddef8669df9151a5f31a2871dfb33a5
Daniel Diniz <daniel.diniz1@icloud.com>
2022-11-15 19:59:05 -0300

    first commit

---
+++ src/main/java/br/com/training/batch/batchconfig/BatchConfig.java
@@ -0,0 +1,59 @@
+package br.com.training.batch.batchconfig;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig extends DefaultBatchConfigurer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Override
+    public void setDataSource(DataSource dataSource) {
+
+    }
+
+    @Bean
+    public Job imprimeOJob(){
+        return  jobBuilderFactory
+                .get("imprimeOJob")
+                .start(imprimeOlaStep())
+                .build();
+    }
+
+    public Step imprimeOlaStep(){
+        return stepBuilderFactory
+                .get("imprimeOJob")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        LOGGER.info("Olá, Mundo!");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+}
---
+++ src/main/java/br/com/training/batch/profile/DevProfile.java
@@ -0,0 +1,11 @@
+package br.com.training.batch.profile;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevProfile {
+
+
+}
