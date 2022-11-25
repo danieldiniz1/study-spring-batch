0b7eb51dcdd3d01958a3115131e8a32b654296e7
Daniel Diniz <daniel.diniz1@icloud.com>
2022-11-25 16:02:09 -0300

    adicionando parametros para inicialização do batch

--- src/main/java/br/com/training/batch/batchconfig/BatchConfig.java
+++ src/main/java/br/com/training/batch/batchconfig/BatchConfig.java
@@ -5,15 +5,13 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,21 +35,27 @@ public class BatchConfig extends DefaultBatchConfigurer {
         return  jobBuilderFactory
                 .get("imprimeOJob")
                 .start(imprimeOlaStep())
-                .incrementer(new RunIdIncrementer()) // permite a execução de um job mais de uma vez sempre incrementando novas infos
+                .incrementer(new RunIdIncrementer()) // permite a execucao de um job mais de uma vez sempre incrementando novas infos
                 .build();
     }
 
     public Step imprimeOlaStep(){
         return stepBuilderFactory
                 .get("imprimeOJob")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        LOGGER.info("Olá, Mundo!");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+                .tasklet(imprimeOlaTasklet(null))
                 .build();
+    }
+
+    @Bean // tem que ser um bean disponível para o spring
+    @StepScope // coloca método no contexto para pegar o parametro
+    public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) { // valor é buscado de um parametro quando app inicializa
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                LOGGER.info("Olá, " + nome +  "!");
+                return RepeatStatus.FINISHED;
+            }
+        };
     }
 
 }
